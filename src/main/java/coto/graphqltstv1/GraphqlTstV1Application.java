01e1cf66dcefffbfadefd40c0aabab6691b2ef72
Krzysztof Sazon <krzysztof.sazon@gmail.com>
2023-08-30 17:07:04 -0400

    init

---
+++ src/main/java/coto/graphqltstv1/GraphqlTstV1Application.java
@@ -0,0 +1,13 @@
+package coto.graphqltstv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlTstV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlTstV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/coto/graphqltstv1/GraphqlTstV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package coto.graphqltstv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlTstV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
